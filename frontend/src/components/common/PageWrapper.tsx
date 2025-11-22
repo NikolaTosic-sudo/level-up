01b00932510476ccbd28135670c574c17858129c
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2025-11-22 16:59:21 +0100

    Tested translations, they work

--- frontend/src/components/common/PageWrapper.tsx
+++ frontend/src/components/common/PageWrapper.tsx
@@ -1,7 +1,11 @@
 import { Outlet } from "@tanstack/react-router";
 
+import '../../../i18n.ts'
+
 function PageWrapper() {
-	return <Outlet />;
+	return (
+		<Outlet />
+	);
 }
 
 export default PageWrapper;
--- frontend/src/routes/about.tsx
+++ frontend/src/routes/about.tsx
@@ -1,9 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useTranslation } from "react-i18next";
 
 export const Route = createFileRoute("/about")({
 	component: AboutComponent,
 });
 
 function AboutComponent() {
-	return <div>About me!</div>;
+	const { t } = useTranslation();
+	return <div>About me!{t("test")}</div>;
 }
