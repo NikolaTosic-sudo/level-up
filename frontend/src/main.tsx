d557bc1b1940d7456f367351f9fe5abd5b31cc25
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2025-11-22 11:40:04 +0100

    Added router to vite plugins and created Router in main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen.ts'
+
+const router = createRouter({
+	routeTree,
+	defaultPreload: 'intent',
+	scrollRestoration: true
+})
+
+declare module '@tanstack/react-router' {
+	interface Register {
+		router: typeof router
+	}
+}
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
 )
