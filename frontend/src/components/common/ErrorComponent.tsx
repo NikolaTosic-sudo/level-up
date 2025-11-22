cc9e5ca6a35624a8989dbb9d0a701b431975409e
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2025-11-22 15:48:02 +0100

    Added Antd library and implemented it in ErroComponent

--- frontend/src/components/common/ErrorComponent.tsx
+++ frontend/src/components/common/ErrorComponent.tsx
@@ -1,9 +1,11 @@
+import { Result } from "antd";
+
 type ErrorComponentProps = {
 	error: Error;
 };
 
 function ErrorComponent({ error }: ErrorComponentProps) {
-	return <div>There was an error, {error.message}</div>;
+	return <Result status={"404"} title={"Not found"} subTitle={error.message} />;
 }
 
 export default ErrorComponent;
