e97fac49605e9eb3f06774b9d96c38ae0d156f7a
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2025-11-25 08:37:17 +0100

    Started the page configuration

--- frontend/src/components/common/PageWrapper.tsx
+++ frontend/src/components/common/PageWrapper.tsx
@@ -1,10 +1,19 @@
+import { Suspense } from "react";
+import { Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
 import { Outlet } from "@tanstack/react-router";
 
-import '../../../i18n.ts'
+import "../../../i18n.ts";
 
 function PageWrapper() {
 	return (
-		<Outlet />
+		<Suspense
+			fallback={<Spin indicator={<LoadingOutlined spin />} size={"large"} />}
+		>
+			<div className="page">
+				<Outlet />
+			</div>
+		</Suspense>
 	);
 }
 
