cd47783c5a8ec38e0796bf94e7ebf55625a415a1
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2025-11-25 08:36:41 +0100

    Added a utils directory

---
+++ frontend/src/utils/utils.ts
@@ -0,0 +1,9 @@
+import { TestTag1Api, type MainTestResponse } from "./api";
+
+export function testFetch(): Promise<MainTestResponse> {
+	const api = new TestTag1Api();
+
+	const result: Promise<MainTestResponse> = api.testGet();
+
+	return result;
+}
