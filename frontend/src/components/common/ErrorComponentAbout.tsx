e1a66600f47a4f693e4fc96a7f9828363ac5e5a2
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2025-11-25 08:36:20 +0100

    Deleted unnecessary files

--- frontend/src/components/common/ErrorComponentAbout.tsx
+++
@@ -1,9 +0,0 @@
-type ErrorComponentProps = {
-	error: Error;
-};
-
-function ErrorComponent({ error }: ErrorComponentProps) {
-	return <div>There was an error, {error.message}, about component</div>;
-}
-
-export default ErrorComponent;
