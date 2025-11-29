f6a744e0a2873eefed4cb93988f856624b9b3e61
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2025-11-29 11:36:05 +0100

    Added basic styling and started configuring theme

--- frontend/src/components/common/PageWrapper.tsx
+++ frontend/src/components/common/PageWrapper.tsx
@@ -1,18 +1,30 @@
 import { Suspense } from "react";
-import { Spin } from "antd";
+import { ConfigProvider, Spin, type ThemeConfig } from "antd";
 import { LoadingOutlined } from "@ant-design/icons";
 import { Outlet } from "@tanstack/react-router";
 
 import "../../../i18n.ts";
+import "./styles/style.css";
+
+const theme: ThemeConfig = {
+	token: {
+		borderRadius: 0,
+		fontFamily: "Montserrat, sans-serif",
+		colorError: "#9B0A3C",
+		colorPrimary: "#008c95",
+	},
+};
 
 function PageWrapper() {
 	return (
 		<Suspense
 			fallback={<Spin indicator={<LoadingOutlined spin />} size={"large"} />}
 		>
-			<div className="page">
-				<Outlet />
-			</div>
+			<ConfigProvider theme={theme}>
+				<div className="page">
+					<Outlet />
+				</div>
+			</ConfigProvider>
 		</Suspense>
 	);
 }
