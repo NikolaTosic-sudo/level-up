1c9d4521981ade289fc451c1eef2b503d69011d2
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2025-12-14 22:56:12 +0100

    Removing gin from main

--- backend/main.go
+++ backend/main.go
@@ -2,15 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-	"time"
 
 	_ "github.com/NikolaTosic-sudo/level-up/backend/docs"
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 type TestResponse struct {
@@ -23,29 +16,7 @@ type TestResponse struct {
 // @host localhost:8080
 func main() {
 
-	r := gin.Default()
 	fmt.Println("started listening on port 8080")
-
-	// Configure CORS
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:5173"} // Specify allowed origins
-	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
-	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization", "Accept", "User-Agent", "Cache-Control", "Pragma"}
-	config.ExposeHeaders = []string{"Content-Length"}
-	config.AllowCredentials = true
-	config.MaxAge = 12 * time.Hour // Cache preflight requests for 12 hours
-
-	// Apply the CORS middleware
-	r.Use(cors.New(config))
-
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	r.GET("/test", testApi)
-
-	err := r.Run()
-
-	if err != nil {
-		log.Println("It broke", err)
-	}
 }
 
 // @Tags test tag 1
@@ -54,10 +25,3 @@ func main() {
 // @Produce json
 // @Success 200 {object} TestResponse
 // @Router /test [get]
-func testApi(ctx *gin.Context) {
-	response := TestResponse{
-		Message: "great job you",
-	}
-
-	ctx.JSON(http.StatusOK, response)
-}
