349814dad0d876a10d43ee1c5b89aae315fa8bbe
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-01-21 08:12:30 +0100

    Updated ProfileCreationWrapper

--- frontend/src/components/profileCreation/ProfileCreation.tsx
+++ frontend/src/components/profileCreation/ProfileCreation.tsx
@@ -1,16 +1,9 @@
-import ProfileCreationForm from "./components/ProfileCreationForm";
-import { ProfileCreationSteps } from "./components/ProfileCreationSteps";
-import { ProfileCreationWrapper } from "./components/ProfileCreationWrapper";
+import ProfileCreationWrapper from "./components/ProfileCreationWrapper";
 
 import "./styles/style.css";
 
 function ProfileCreation() {
-  return (
-    <ProfileCreationWrapper>
-      <ProfileCreationSteps />
-      <ProfileCreationForm />
-    </ProfileCreationWrapper>
-  );
+  return <ProfileCreationWrapper />;
 }
 
 export default ProfileCreation;
--- frontend/src/components/profileCreation/components/ProfileCreationWrapper.tsx
+++ frontend/src/components/profileCreation/components/ProfileCreationWrapper.tsx
@@ -1,11 +1,59 @@
-import type { ReactNode } from "react";
+import { Button, Flex } from "antd";
+import {
+  CurrentStepsEnum,
+  useProfileCreationStore,
+} from "../store/useProfileCreationStore";
+import ProfileCreationForm from "./ProfileCreationForm";
+import { ProfileCreationSteps } from "./ProfileCreationSteps";
+import { useTranslation } from "react-i18next";
 
-type ProfileCreationWrapperProps = {
-  children: ReactNode;
+const ProfileCreationWrapper = () => {
+  const { t } = useTranslation();
+
+  const { currentStep, setCurrentStep } = useProfileCreationStore();
+
+  function handleNextStep() {
+    if (currentStep !== CurrentStepsEnum.Finish) {
+      setCurrentStep(currentStep + 1);
+    } else {
+      alert("finished");
+    }
+  }
+
+  return (
+    <div className="profile-creation-wrapper">
+      <ProfileCreationSteps />
+
+      {currentStep === CurrentStepsEnum.Form ? (
+        <ProfileCreationForm />
+      ) : currentStep === CurrentStepsEnum.Skills ? (
+        <div>Skills</div>
+      ) : currentStep === CurrentStepsEnum.Quests ? (
+        <div>Quests</div>
+      ) : (
+        <div>Finish</div>
+      )}
+
+      <Flex justify="end" gap={8}>
+        {currentStep !== CurrentStepsEnum.Form && (
+          <Button onClick={() => setCurrentStep(currentStep - 1)} danger>
+            {t("profileCreationTrans.stepBtn.previous", {
+              defaultValue: "Previous step",
+            })}
+          </Button>
+        )}
+        <Button type="primary" onClick={handleNextStep}>
+          {currentStep !== CurrentStepsEnum.Finish
+            ? t("profileCreationTrans.stepBtn.next", {
+                defaultValue: "Next step",
+              })
+            : t("profileCreationTrans.stepBtn.finish", {
+                defaultValue: "Finish",
+              })}
+        </Button>
+      </Flex>
+    </div>
+  );
 };
 
-export const ProfileCreationWrapper = ({
-  children,
-}: ProfileCreationWrapperProps) => {
-  return <div className="profile-creation-wrapper">{children}</div>;
-};
+export default ProfileCreationWrapper;
