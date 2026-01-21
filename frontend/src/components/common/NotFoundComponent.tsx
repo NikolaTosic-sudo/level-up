ac7f249c40089719fda3f00f4b509cbd0bfc82b1
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-01-21 07:19:07 +0100

    Not found component and leftover files from api generation

---
+++ frontend/src/components/common/NotFoundComponent.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+type NotFoundComponentProps = {};
+
+function NotFoundComponent({}): NotFoundComponentProps {
+  return <div></div>;
+}
+
+export type { NotFoundComponentProps };
+export default NotFoundComponent;
