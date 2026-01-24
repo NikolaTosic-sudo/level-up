0bedcbbc357f9e65152b67115d5a9ac08c1e050d
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-01-24 14:32:28 +0100

    Added all values to be shown in Finish step

--- frontend/src/components/profileCreation/components/ProfileCreationFinish.tsx
+++ frontend/src/components/profileCreation/components/ProfileCreationFinish.tsx
@@ -1,3 +1,11 @@
+import { Form } from "antd";
+
 export const ProfileCreationFinish = () => {
+  const formInstance = Form.useFormInstance();
+
+  const values = formInstance.getFieldsValue();
+
+  console.log(values);
+
   return <div></div>;
 };
