fcc5b20b8cf859ad909152dc10d228815c1de4c3
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-02-09 14:58:40 +0100

    Updated landing page

--- frontend/src/components/landingPage/LandingPage.tsx
+++ frontend/src/components/landingPage/LandingPage.tsx
@@ -2,14 +2,22 @@ import { Button, Modal } from "antd";
 import { useState } from "react";
 
 function LandingPage() {
-	const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-	return (
-		<>
-			<Button type="primary" onClick={() => setOpen(true)}>Open me</Button>
-			<Modal cancelButtonProps={{ danger: true, type: "primary" }} onCancel={() => setOpen(false)} open={open}>Test Modal</Modal>
-		</>
-	)
+  return (
+    <>
+      <Button type="primary" onClick={() => setOpen(true)}>
+        Open me now!
+      </Button>
+      <Modal
+        cancelButtonProps={{ danger: true, type: "primary" }}
+        onCancel={() => setOpen(false)}
+        open={open}
+      >
+        Test Modal
+      </Modal>
+    </>
+  );
 }
 
 export default LandingPage;
