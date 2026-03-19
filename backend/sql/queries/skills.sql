c5c8364b03644712a2735b01d6073439d4031bb0
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-03-19 18:11:07 +0100

    Updated skills schema and created queries for skills
    - Made id to be an integer and to be serialized, also name unique
    - Queries to get all skills, and skills by name
    - Query to create a skill

---
+++ backend/sql/queries/skills.sql
@@ -0,0 +1,9 @@
+-- name: GetAllSkills :many
+SELECT name FROM skills ORDER BY name LIMIT 200;
+
+-- name: GetSkillsByName :many
+SELECT name FROM skills WHERE name LIKE $1 ORDER BY name LIMIT 200;
+
+-- name: CreateSkill :exec
+INSERT INTO skills(created_at, updated_at, name) VALUES (NOW(), NOW(), $1);
+
