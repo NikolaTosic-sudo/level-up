1843125b19952528a32fb3b50de92f3e4a8b6abf
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-03-21 20:44:25 +0100

    removed FormData from store as it is unnecessary

--- frontend/src/components/profileCreation/store/useProfileCreationStore.tsx
+++ frontend/src/components/profileCreation/store/useProfileCreationStore.tsx
@@ -1,4 +1,3 @@
-import type { Dayjs } from "dayjs";
 import { create } from "zustand";
 
 // investigate the erasableSyntaxOnly property on Typescript
@@ -9,35 +8,17 @@ export enum CurrentStepsEnum {
   Finish,
 }
 
-interface FormData {
-  firstName: string;
-  lastName: string;
-  nickname: string;
-  dateOfBirth: Dayjs | null;
-}
-
 interface ProfileCreationStore {
   loading: boolean;
   disabled: boolean;
-
-  formData: FormData;
 
   currentStep: number;
   setCurrentStep: (currentStep: number) => void;
 }
 
-const InitalFormData = {
-  firstName: "",
-  lastName: "",
-  nickname: "",
-  dateOfBirth: null,
-};
-
 export const useProfileCreationStore = create<ProfileCreationStore>((set) => ({
   loading: false,
   disabled: false,
-
-  formData: InitalFormData,
 
   currentStep: 0,
   setCurrentStep: (currentStep) => set({ currentStep }),
