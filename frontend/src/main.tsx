62909702932a082ae411618cfd7c6a7b6e161004
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-03-24 11:59:08 +0100

    Prettier main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,30 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createRouter, RouterProvider } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen.ts'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 const router = createRouter({
-	routeTree,
-	defaultPreload: 'intent',
-	scrollRestoration: true,
-	context: {
-		queryClient,
-	}
-})
+  routeTree,
+  defaultPreload: "intent",
+  scrollRestoration: true,
+  context: {
+    queryClient,
+  },
+});
 
-declare module '@tanstack/react-router' {
-	interface Register {
-		router: typeof router
-	}
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
 }
 
-createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<RouterProvider router={router} />
-		</QueryClientProvider>
-	</StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>,
+);
