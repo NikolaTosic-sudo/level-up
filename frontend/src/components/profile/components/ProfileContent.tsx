4fdee4c5e878752df6785a748b5ad32e377cfcad
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-03-26 14:17:31 +0100

    Separated ProfileContent and ProfileHeader components

---
+++ frontend/src/components/profile/components/ProfileContent.tsx
@@ -0,0 +1,9 @@
+import { Layout } from "antd";
+
+const { Content } = Layout;
+
+function ProfileContent() {
+  return <Content className="profile-content">Test content</Content>;
+}
+
+export default ProfileContent;
---
+++ frontend/src/components/profile/components/ProfileHeader.tsx
@@ -0,0 +1,9 @@
+import { Layout } from "antd";
+
+const { Header } = Layout;
+
+function ProfileHeader() {
+  return <Header className="profile-header">Test header</Header>;
+}
+
+export default ProfileHeader;
