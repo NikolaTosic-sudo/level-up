f2d9d1d398be05ac4d8a21b3576c6e0cc68cc012
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-03-29 20:39:49 +0200

    Started with developing header

--- frontend/src/components/profile/components/ProfileHeader.tsx
+++ frontend/src/components/profile/components/ProfileHeader.tsx
@@ -1,9 +1,36 @@
-import { Layout } from "antd";
+import { BellOutlined } from "@ant-design/icons";
+import { Dropdown, Layout, type MenuProps } from "antd";
 
 const { Header } = Layout;
 
 function ProfileHeader() {
-  return <Header className="profile-header">Test header</Header>;
+  const items: MenuProps["items"] = [
+    {
+      key: "1",
+      label: "first item",
+    },
+
+    {
+      key: "2",
+      label: "second item",
+    },
+
+    {
+      key: "3",
+      label: "third item",
+    },
+  ];
+
+  return (
+    <Header className="profile-header">
+      Test header
+      <Dropdown menu={{ items }}>
+        <a onClick={(e) => e.preventDefault()}>
+          <BellOutlined />
+        </a>
+      </Dropdown>
+    </Header>
+  );
 }
 
 export default ProfileHeader;
