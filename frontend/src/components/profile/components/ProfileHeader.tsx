9a39a02085254bd21e6f7a3acb3afb2b5f43a193
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-03-30 19:59:56 +0200

    Added "progress", which will represent level and exp

--- frontend/src/components/profile/components/ProfileHeader.tsx
+++ frontend/src/components/profile/components/ProfileHeader.tsx
@@ -1,4 +1,4 @@
-import { Layout } from "antd";
+import { Layout, Progress } from "antd";
 import ProfileNotifications from "./ProfileNotifications";
 
 const { Header } = Layout;
@@ -7,6 +7,19 @@ function ProfileHeader() {
   return (
     <Header className="profile-header">
       <ProfileNotifications />
+      <Progress
+        percent={60}
+        showInfo={false}
+        strokeColor="#008c95"
+        styles={{
+          rail: {
+            height: 10,
+          },
+          track: {
+            height: 10,
+          },
+        }}
+      />
     </Header>
   );
 }
