4c5ccc9f59517d2101e4c0e663792e534458849f
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-03-30 15:00:16 +0200

    Additional styling for notifications
    - added "title" and experimented with extra and icon

--- frontend/src/components/profile/components/ProfileNotifications.tsx
+++ frontend/src/components/profile/components/ProfileNotifications.tsx
@@ -1,11 +1,14 @@
-import { BellOutlined } from "@ant-design/icons";
-import { Badge, Dropdown, type MenuProps } from "antd";
+import { BellOutlined, DeliveredProcedureOutlined } from "@ant-design/icons";
+import { Badge, Divider, Dropdown, type MenuProps } from "antd";
 
 function ProfileNotifications() {
   const items: MenuProps["items"] = [
     {
       key: "1",
       label: "first item",
+      title: "Ovo je title",
+      icon: <DeliveredProcedureOutlined />,
+      extra: <div>extra je ovo</div>,
     },
 
     {
@@ -20,8 +23,18 @@ function ProfileNotifications() {
   ];
 
   return (
-    <Dropdown menu={{ items }} trigger={["click"]}>
-      <Badge count={items.length} size="small">
+    <Dropdown
+      menu={{ items }}
+      trigger={["click"]}
+      popupRender={(menu) => (
+        <>
+          <div>Notifications</div>
+          <Divider size="middle" />
+          {menu}
+        </>
+      )}
+    >
+      <Badge count={items.length} size="small" color="#008c95">
         <BellOutlined style={{ fontSize: 18 }} />
       </Badge>
     </Dropdown>
