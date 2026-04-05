bc7dc14af58d987bf10b9f9c018c8a730c9efdd9
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-04-05 14:56:06 +0200

    expanded notification icons with a new type

--- frontend/src/components/profile/components/NotificationsIcon.tsx
+++ frontend/src/components/profile/components/NotificationsIcon.tsx
@@ -1,12 +1,13 @@
 import { GiftOutlined, VideoCameraOutlined } from "@ant-design/icons";
 
 type NotificationsIconProps = {
-  iconType: "reward" | "task"; // will be expanded
+  iconType: "reward" | "task" | "failure"; // will be expanded
 };
 
 const Icons = {
   reward: <GiftOutlined />,
   task: <VideoCameraOutlined />,
+  failure: <div />,
 };
 
 const NotificationsIcon = ({ iconType }: NotificationsIconProps) => {
