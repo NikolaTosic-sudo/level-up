386ae542f3101d360729e3cb83be0e7371756091
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-04-08 15:37:08 +0200

    Added tooltip for Shop button/modal

--- frontend/src/components/profile/components/ProfileShop.tsx
+++ frontend/src/components/profile/components/ProfileShop.tsx
@@ -1,21 +1,51 @@
 import { DollarCircleOutlined } from "@ant-design/icons";
-import { Button, Modal } from "antd";
+import { Button, Divider, Modal, Tooltip, Typography } from "antd";
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 function ProfileShop() {
+  const { t } = useTranslation();
   const [open, setOpen] = useState(false);
 
   return (
     <>
-      <Button type="text" onClick={() => setOpen(true)}>
-        <DollarCircleOutlined style={{ fontSize: 18 }} />
-      </Button>
+      <Tooltip
+        title={t("profile.header.shop.tooltip", { defaultValue: "Shop" })}
+      >
+        <Button type="text" onClick={() => setOpen(true)}>
+          <DollarCircleOutlined style={{ fontSize: 18 }} />
+        </Button>
+      </Tooltip>
 
       <Modal
         cancelButtonProps={{ danger: true, type: "primary" }}
         onCancel={() => setOpen(false)}
         open={open}
+        closable={false}
+        title={
+          <>
+            <Typography.Title level={4}>
+              {t("profile.header.shop.title", { defaultValue: "Shop" })}
+            </Typography.Title>
+            <Divider />
+          </>
+        }
       >
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
         <div>test</div>
       </Modal>
     </>
