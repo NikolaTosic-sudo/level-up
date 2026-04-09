7c6fcce18d6dc3d9f76195646a2f84fdaac8fd5e
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-04-09 20:02:33 +0200

    Moved Progress to it's own component

--- frontend/src/components/profile/components/ProfileHeader.tsx
+++ frontend/src/components/profile/components/ProfileHeader.tsx
@@ -1,6 +1,7 @@
-import { Layout, Progress, Tooltip } from "antd";
+import { Layout } from "antd";
 import ProfileNotifications from "./ProfileNotifications";
 import ProfileShop from "./ProfileShop";
+import ProfileProgress from "./ProfileProgress";
 
 const { Header } = Layout;
 
@@ -9,17 +10,7 @@ function ProfileHeader() {
     <Header className="profile-header">
       <ProfileNotifications />
       <ProfileShop />
-      <Tooltip title="60 / 100 XP">
-        <Progress
-          percent={(60 / 100) * 100}
-          showInfo={false}
-          strokeColor="#008c95"
-          size={{ height: 10 }}
-        />
-      </Tooltip>
-      <Tooltip title="60 / 100 XP">
-        <span style={{ marginLeft: -12 }}>70</span>
-      </Tooltip>
+      <ProfileProgress />
     </Header>
   );
 }
---
+++ frontend/src/components/profile/components/ProfileProgress.tsx
@@ -0,0 +1,21 @@
+import { Progress, Tooltip } from "antd";
+
+function ProfileProgress() {
+  return (
+    <>
+      <Tooltip title="60 / 100 XP">
+        <Progress
+          percent={(60 / 100) * 100}
+          showInfo={false}
+          strokeColor="#008c95"
+          size={{ height: 10 }}
+        />
+      </Tooltip>
+      <Tooltip title="60 / 100 XP">
+        <span style={{ marginLeft: -12 }}>70</span>
+      </Tooltip>
+    </>
+  );
+}
+
+export default ProfileProgress;
