cad6681ba87d7efd1b60c9db12fc742ab63a2f83
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-04-16 14:46:15 +0200

    Added Skills component to ProfileContent

--- frontend/src/components/profile/components/ProfileContent.tsx
+++ frontend/src/components/profile/components/ProfileContent.tsx
@@ -1,9 +1,24 @@
 import { Layout } from "antd";
+import { useProfileWrapStore } from "../store/useProfileWrapStore";
+import Skills from "../../skills/Skills";
+import { Activity } from "react";
 
 const { Content } = Layout;
 
 function ProfileContent() {
-  return <Content className="profile-content">Test content</Content>;
+  const { currentModule } = useProfileWrapStore();
+
+  return (
+    <Content className="profile-content">
+      <Activity mode={currentModule === "skills" ? "visible" : "hidden"}>
+        <Skills />
+      </Activity>
+
+      <Activity mode={currentModule !== "skills" ? "visible" : "hidden"}>
+        Test content
+      </Activity>
+    </Content>
+  );
 }
 
 export default ProfileContent;
