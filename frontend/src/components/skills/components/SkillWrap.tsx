1d5206f66c905fd39c614985134d878c5b5efc9a
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-04-28 03:13:17 +0200

    Added a separate wrapper component for Skills

---
+++ frontend/src/components/skills/components/SkillWrap.tsx
@@ -0,0 +1,63 @@
+import { Button, Divider, Flex, Popconfirm, Progress, Tooltip } from "antd";
+import type { Skill } from "./SkillModal";
+import { useHover } from "../../../hooks/useHover";
+import SkillModal from "./SkillModal";
+import { DeleteOutlined } from "@ant-design/icons";
+import { useTranslation } from "react-i18next";
+
+type SkillWrapProps = {
+  skill: Skill;
+};
+
+function SkillWrap({ skill }: SkillWrapProps) {
+  const { t } = useTranslation();
+  const { isHovered, bind } = useHover();
+
+  return (
+    <div {...bind} style={{ position: "relative" }}>
+      <Flex
+        className="skill-wrap"
+        style={{ paddingBottom: isHovered ? 16 : 8 }}
+        align="center"
+      >
+        <span style={{ color: "white" }}>{skill.name}</span>
+        <Divider vertical style={{ marginBlock: -12 }} />
+        <Tooltip
+          title={`${skill.experience} / ${skill.experienceNeeded} exp - level ${skill.level}`}
+          style={{ display: "flex" }}
+        >
+          <Progress
+            showInfo={false}
+            percent={(skill.experience / skill.experienceNeeded) * 100}
+            strokeColor="#008c95"
+            style={{ minWidth: 60 }}
+          />
+        </Tooltip>
+        <span style={{ marginLeft: 6, color: "white" }}>{skill.level}</span>
+      </Flex>
+
+      {isHovered ? (
+        <div className="skill-actions">
+          <SkillModal skill={skill} />
+          <Popconfirm
+            title={t("", {
+              defaultValue: "Are you sure you want to remove this skill?",
+            })}
+            cancelButtonProps={{ danger: true, type: "primary" }}
+          >
+            <Tooltip title={t("", { defaultValue: "Remove skill" })}>
+              <Button
+                style={{ fontSize: 10, height: 20, width: 20 }}
+                ghost
+                danger
+                icon={<DeleteOutlined />}
+              />
+            </Tooltip>
+          </Popconfirm>
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default SkillWrap;
--- frontend/src/components/skills/components/SkillsWrapper.tsx
+++ frontend/src/components/skills/components/SkillsWrapper.tsx
@@ -1,31 +1,15 @@
 import { Flex } from "antd";
 import skills from "./skills.json";
-import SkillModal from "./SkillModal";
-import ModalComponent from "../../common/ModalComponent";
-import { PlusOutlined } from "@ant-design/icons";
-import { useTranslation } from "react-i18next";
-import SkillForm from "./SkillForm";
+import SkillWrap from "./SkillWrap";
+import NewSkillWrapper from "./NewSkillWrapper";
 
 function SkillsWrapper() {
-  const { t } = useTranslation();
-
   return (
     <>
-      <ModalComponent
-        buttonProps={{
-          type: "primary",
-          icon: <PlusOutlined />,
-        }}
-        buttonInner={t("", { defaultValue: "Add skill" })}
-        cancelButtonProps={{ danger: true, type: "primary" }}
-        okText={t("", { defaultValue: "Save" })}
-        destroyOnHidden
-      >
-        <SkillForm />
-      </ModalComponent>
+      <NewSkillWrapper />
       <Flex gap={16} wrap style={{ marginTop: 16 }}>
         {skills.map((s) => (
-          <SkillModal key={s.id} skill={s} />
+          <SkillWrap key={s.id} skill={s} />
         ))}
       </Flex>
     </>
