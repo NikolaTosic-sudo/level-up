b262dc1126d9f8e6f0c4972cc980c8ab2749771e
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-04-28 04:19:53 +0200

    Started with Quests

---
+++ frontend/src/components/tasks/Quests.tsx
@@ -0,0 +1,9 @@
+import QuestsWrapper from "./components/QuestsWrapper";
+
+import "./styles/styles.css";
+
+function Quests() {
+  return <QuestsWrapper />;
+}
+
+export default Quests;
---
+++ frontend/src/components/tasks/components/QuestsWrapper.tsx
@@ -0,0 +1,50 @@
+import { Collapse } from "antd";
+import ModalComponent from "../../common/ModalComponent";
+import { useTranslation } from "react-i18next";
+import { PlusOutlined } from "@ant-design/icons";
+
+function QuestsWrapper() {
+  const { t } = useTranslation();
+  return (
+    <>
+      <Collapse
+        items={[
+          {
+            key: "1",
+            label: "Repeating quests",
+            children: <div>Yes</div>,
+            extra: (
+              <ModalComponent
+                buttonInner={t("", { defaultValue: "Add a repeating quest" })}
+                buttonProps={{
+                  type: "primary",
+                  icon: <PlusOutlined />,
+                }}
+              ></ModalComponent>
+            ),
+          },
+        ]}
+      />
+      <Collapse
+        items={[
+          {
+            key: "2",
+            label: "Custom quests",
+            children: <div>No</div>,
+            extra: (
+              <ModalComponent
+                buttonInner={t("", { defaultValue: "Add a custom quest" })}
+                buttonProps={{
+                  type: "primary",
+                  icon: <PlusOutlined />,
+                }}
+              ></ModalComponent>
+            ),
+          },
+        ]}
+      />
+    </>
+  );
+}
+
+export default QuestsWrapper;
