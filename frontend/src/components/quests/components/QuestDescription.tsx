ffefe5b8262a26996ba649af2cd43bd316013b84
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-05-03 23:41:31 +0200

    Using translations in quest descriptions

--- frontend/src/components/quests/components/QuestDescription.tsx
+++ frontend/src/components/quests/components/QuestDescription.tsx
@@ -1,15 +1,18 @@
 import { Descriptions } from "antd";
 import type { Quest } from "./Quest";
 import type { DescriptionsItemProps } from "antd/es/descriptions/Item";
+import { useTranslation } from "react-i18next";
 
 type QuestDescrptionProps = {
   quest: Quest;
 };
 
 function QuestDescrption({ quest }: QuestDescrptionProps) {
+  const { t } = useTranslation();
+
   const items: DescriptionsItemProps[] = [
     {
-      label: "Experience to be gained",
+      label: t("", { defaultValue: "Experience to be gained" }),
       children: quest.experience,
     },
   ];
