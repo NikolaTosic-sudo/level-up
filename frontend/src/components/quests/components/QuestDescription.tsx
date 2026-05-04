aea2fc4ce5812c87e7599592105a1390f849fb83
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-05-04 23:46:08 +0200

    Continuing with Description of the Quest

--- frontend/src/components/quests/components/QuestDescription.tsx
+++ frontend/src/components/quests/components/QuestDescription.tsx
@@ -1,7 +1,8 @@
-import { Descriptions } from "antd";
+import { Button, Card, Descriptions, Flex, Popconfirm } from "antd";
 import type { Quest } from "./Quest";
 import type { DescriptionsItemProps } from "antd/es/descriptions/Item";
 import { useTranslation } from "react-i18next";
+import { CheckOutlined, DeleteOutlined } from "@ant-design/icons";
 
 type QuestDescrptionProps = {
   quest: Quest;
@@ -12,14 +13,74 @@ function QuestDescrption({ quest }: QuestDescrptionProps) {
 
   const items: DescriptionsItemProps[] = [
     {
+      label: t("quest.description.subquests", {
+        defaultValue: "Sub-Quests",
+      }),
+      children: quest?.subTasks?.map((q) => (
+        <div style={{ display: "block" }}>
+          <Card
+            variant="borderless"
+            title={q.title}
+            styles={{ title: { fontSize: 16 } }}
+            extra={
+              <Flex gap={16}>
+                <Popconfirm
+                  title={t("quest.subquest.confirm.done", {
+                    defaultValue: "Are you done with this Subtquest?",
+                  })}
+                >
+                  <Button
+                    variant="outlined"
+                    color="green"
+                    ghost
+                    icon={<CheckOutlined style={{ fontSize: 12 }} />}
+                    onClick={(e) => e.stopPropagation()}
+                  />
+                </Popconfirm>
+                <Popconfirm
+                  title={t("quest.subquest.confirm.delete", {
+                    defaultValue: "Are you sure you want to delete this quest?",
+                  })}
+                >
+                  <Button
+                    ghost
+                    danger
+                    icon={<DeleteOutlined style={{ fontSize: 12 }} />}
+                    onClick={(e) => e.stopPropagation()}
+                  />
+                </Popconfirm>
+              </Flex>
+            }
+          />
+        </div>
+      )),
+      span: 3,
+      style: quest.subTasks && quest.subTasks.length ? {} : { display: "none" },
+      styles: { content: { width: "100%", display: "block" } },
+    },
+    {
       label: t("quest.description.exp", {
         defaultValue: "Experience to be gained",
       }),
       children: quest.experience,
     },
+    {
+      label: t("quest.description.skills", {
+        defaultValue: "Skills to be improved",
+      }),
+      children: quest.skills?.map((s) => s.name).join(", "),
+      style: quest.skills && quest.skills.length ? {} : { display: "none" },
+    },
   ];
 
-  return <Descriptions title={quest.title} items={items} />;
+  return (
+    <Descriptions
+      layout="vertical"
+      column={3}
+      title={quest.title}
+      items={items}
+    />
+  );
 }
 
 export default QuestDescrption;
