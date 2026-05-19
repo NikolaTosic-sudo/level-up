17970365ad821d0f80f62402d3563eaf74314138
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-05-19 23:51:47 +0200

    implemented Sign In for testing

--- frontend/src/components/landingPage/LandingPage.tsx
+++ frontend/src/components/landingPage/LandingPage.tsx
@@ -1,15 +1,20 @@
 import ModalComponent from "../common/ModalComponent";
+import SignIn from "../signIn/SignIn";
 
 function LandingPage() {
   return (
-    <ModalComponent
-      buttonInner="Test"
-      buttonProps={{
-        type: "primary",
-      }}
-    >
-      Test Modal
-    </ModalComponent>
+    <>
+      <ModalComponent
+        buttonInner="Test"
+        buttonProps={{
+          type: "primary",
+        }}
+      >
+        Test Modal
+      </ModalComponent>
+
+      <SignIn />
+    </>
   );
 }
 
