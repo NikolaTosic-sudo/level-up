368dbc85cdfb5112d95e08b242a6cd78e99d7b7c
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-05-27 01:13:36 +0200

    Error component for the dashboard loader

--- frontend/src/components/dashboard/components/DashboardLoader.tsx
+++ frontend/src/components/dashboard/components/DashboardLoader.tsx
@@ -1,9 +1,14 @@
+import ErrorComponent from "../../common/ErrorComponent";
 import LoadingComponent from "../../common/LoadingComponents";
 import { useGetUser } from "../hooks/useGetUser";
 import DashboardWrapper from "./DashboardWrapper";
 
 function DashboardLoader() {
-  const { data, isLoading } = useGetUser();
+  const { data, isLoading, error, isError } = useGetUser();
+
+  if (isError) {
+    return <ErrorComponent error={error} />;
+  }
 
   if (isLoading) {
     return <LoadingComponent />;
