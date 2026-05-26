25de92acf79e8395e2c1288a084ab378a13fe8d5
NikolaTosic-sudo <toleslaoffice@gmail.com>
2026-05-27 00:25:32 +0200

    Get the user from access token cookie and validate him

---
+++ backend/user.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/NikolaTosic-sudo/level-up/backend/internal/auth"
+	"github.com/google/uuid"
+)
+
+func (cfg *appConfig) getUserId(r *http.Request) (uuid.UUID, error) {
+	userId, err := uuid.NewUUID()
+	if err != nil {
+		return userId, err
+	}
+
+	userC, err := r.Cookie("access_token")
+	if err != nil {
+		fmt.Println("odje?")
+		return userId, err
+	}
+
+	if userC.Value == "" {
+		return userId, fmt.Errorf("invalid access token")
+	}
+
+	userId, err = auth.ValidateJWT(userC.Value, cfg.secret)
+	if err != nil {
+		return userId, err
+	}
+
+	return userId, err
+}
